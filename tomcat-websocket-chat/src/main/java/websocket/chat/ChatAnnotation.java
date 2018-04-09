6f279ca8a9c0df95aeae426553e42f53adb49cd8
esfabrizzio <esfabrizzio@gmail.com>
2018-04-08 18:44:39 -0600

    commit

--- tomcat-websocket-chat/src/main/java/websocket/chat/ChatAnnotation.java
+++ tomcat-websocket-chat/src/main/java/websocket/chat/ChatAnnotation.java
@@ -16,8 +16,31 @@
  */
 package websocket.chat;
 
+import java.io.IOException;
 
-public class ChatAnnotation {
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ChatAnnotation extends HttpServlet{
+
+	private static final long serialVersionUID = 1L;
+	
+	
+	public void init(ServletConfig config) throws ServletException {
+		
+	}
+	
+	
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+	}
+	
+	
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	}
 
  
 }
